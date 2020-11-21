7f3ee3779af91d09645fb4beb0a4f0453652c1b7
Detlolka <detlolka@yahoo.com>
2020-11-21 11:04:28 +0300

    feature: сверстан компонент для отображения новостей

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./Header";
 import SearchFrom from "./SearchForm";
+import NewsCardList from './NewsCardList'
 import About from "./About";
 import Footer from "./Footer";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Header />
         <SearchFrom />
       </div>
+      <NewsCardList />
       <About />
       <Footer />
     </div>
---
+++ src/components/NewsCardList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewCardList() {
+    return (
+        <div className='cardList'>
+        <p className="cardList__result">Результаты поиска</p>
+        </div>
+    )
+}
